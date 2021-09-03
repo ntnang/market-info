fed97a6722065e2c41fe38dcad3574a0f73d162a
ntnang <ntnang91@gmail.com>
2021-09-03 17:14:01 +0700

    adjust chart options

--- src/components/content/dashboard.jsx
+++ src/components/content/dashboard.jsx
@@ -22,20 +22,6 @@ class Dashboard extends Component {
     .reverse();
 
   weekDayNames = ["SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT"];
-  monthNames = [
-    "JAN",
-    "FEB",
-    "MAR",
-    "APR",
-    "MAY",
-    "JUN",
-    "JUL",
-    "AUG",
-    "SEP",
-    "OCT",
-    "NOV",
-    "DEC",
-  ];
 
   componentDidMount() {
     this.findLastTrackedProductHistories();
@@ -129,34 +115,23 @@ class Dashboard extends Component {
 
   gradientChartOptionsConfigurationWithTooltipPurple = {
     maintainAspectRatio: false,
-    legend: {
-      display: false,
-    },
-
     tooltips: {
       backgroundColor: "#f5f5f5",
       titleFontColor: "#333",
       bodyFontColor: "#666",
       bodySpacing: 4,
       xPadding: 12,
-      mode: "nearest",
-      intersect: 0,
-      position: "nearest",
     },
     responsive: true,
     scales: {
       yAxes: [
         {
-          barPercentage: 1.6,
           gridLines: {
             drawBorder: false,
             color: "rgba(29,140,248,0.0)",
             zeroLineColor: "transparent",
           },
           ticks: {
-            suggestedMin: 60,
-            suggestedMax: 125,
-            padding: 20,
             fontColor: "#9a9a9a",
           },
         },
@@ -164,14 +139,12 @@ class Dashboard extends Component {
 
       xAxes: [
         {
-          barPercentage: 1.6,
           gridLines: {
             drawBorder: false,
             color: "rgba(225,78,202,0.1)",
-            zeroLineColor: "transparent",
+            zeroLineColor: "rgba(225,78,202,0.1)",
           },
           ticks: {
-            padding: 20,
             fontColor: "#9a9a9a",
           },
         },
