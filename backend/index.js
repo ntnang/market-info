89e039858682b2b3ece4f6cf05df2621428075b1
ntnang <ntnang91@gmail.com>
2021-10-31 16:36:24 +0700

    fix find last product history api

--- backend/index.js
+++ backend/index.js
@@ -37,17 +37,14 @@ app.get("/api/last-product/history/", (req, res) => {
   ProductHistory.findOne()
     .sort({ lastTrackedDate: -1 })
     .exec((err, lastTrackedProduct) => {
+      if (err) {
+        console.error(err);
+        res.status(500).send(err);
+      }
       if (lastTrackedProduct) {
-        ProductHistory.findOne({ id: lastTrackedProduct.id }).exec(
-          (err, productHistory) => {
-            if (err) {
-              console.error(err);
-              res.status(500).send(err);
-            } else {
-              res.status(200).send(productHistory);
-            }
-          }
-        );
+        res.status(200).send(lastTrackedProduct);
+      } else {
+        res.status(404).send();
       }
     });
 });
